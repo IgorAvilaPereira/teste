b9687cebd61285768993764adf2ebb892aee057a
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 14:44:26 -0300

    teste

--- teste.sh
+++ teste.sh
@@ -1,15 +1,18 @@
 #!/bin/bash  
 
+script="teste"
+
 git add * && git commit -m $1 && git push
 
 echo "# $1 <br>" > "README.md"
-
 echo "# $1 <br>" > "./../$1.wiki/Home.md"
 
 for d in ./*; do
 	if [ "$d" != "./README.md" ]; then
-		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"
-	        echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
+		if ["$d" != "./$script.sh"]; then
+			echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"
+		        echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
+		fi
 	fi	
 done
 
