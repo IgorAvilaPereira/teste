7f4b80d9d1ea8a6067975ca96c792f94051a2176
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 14:30:00 -0300

    aula1

--- teste.sh
+++ teste.sh
@@ -1,12 +1,10 @@
 #!/bin/bash  
 
 git add * && git commit -m $2 && git push
-
 echo "# $1 <br>" > "README.md"
 echo "# $1 <br>" > "./../$1.wiki/Home.md"
 
-search_dir=.
-for entry in "$search_dir"/*
+for entry in  ls -l | grep "^d"
 do
         echo "$entry"
     	#echo "## $2 <br> <br> [Acessar](https://github.com/IgorAvilaPereira/$1/tree/main/$entry) <br>" >> "README.md"
