dbd5fc4f830f57f627205e9ac049ad965cd360bc
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 14:39:31 -0300

    teste

--- teste.sh
+++ teste.sh
@@ -3,12 +3,14 @@
 git add * && git commit -m $1 && git push
 
 echo "# $1 <br>" > "README.md"
+
 echo "# $1 <br>" > "./../$1.wiki/Home.md"
 
-for d in ./*; do      
+for d in ./*; do
+
     	echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"
 	echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
 done
 
-git add * && git commit -m $2 && git push
-cd "./../$1.wiki/" && git add * && git commit -m $2 && git push
+git add * && git commit -m $1 && git push
+cd "./../$1.wiki/" && git add * && git commit -m $1 && git push
