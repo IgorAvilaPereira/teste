26ea96edf26e287d34103ccd333d52c257cc7c23
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 14:02:22 -0300

    aula1

--- teste.sh
+++ teste.sh
@@ -5,7 +5,7 @@ git add * && git commit -m $2 && git push
 search_dir=$2
 for entry in "$search_dir"/*
 do
-    echo $2 >> "./../$1.wiki/Home.md"
+    echo $2 > "./../$1.wiki/Home.md"
 done
 
 cd "./../$1.wiki/" && git add * && git commit -m $2 && git push
