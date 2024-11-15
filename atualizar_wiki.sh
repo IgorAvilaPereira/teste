22f8001ec4cb04acb8746bfd57bfefb5f8e0621e
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 23:47:17 -0300

    teste

--- atualizar_wiki.sh
+++ atualizar_wiki.sh
@@ -10,11 +10,12 @@ for d in ./*; do
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"		
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
 		
-		for d2 in "$d/*"; do
-			echo $d2
+		# for d2 in "$d/*"; do
+			x = ls $d2
+			echo $x
 			# echo "[$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d2) <br>" >> "README.md"		
 			# echo "[$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d2) <br>" >> "./../$1.wiki/Home.md"			
-		done
+		# done
 	fi
 done
 echo "Atualizando o readme"
