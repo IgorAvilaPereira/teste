b196bb1625abc3cbf48bd196a895a6bc7447c645
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 23:28:17 -0300

    ok

--- atualizar_wiki.sh
+++ atualizar_wiki.sh
@@ -7,8 +7,12 @@ echo "# $1 <br>" > "./../$1.wiki/Home.md"
 echo "Listando todos os diretorios de $1"
 for d in ./*; do
 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar_wiki.sh" && "$d" != "./nova_aula.sh" ]]; then
-		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"
+		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"		
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
+		for d2 in ./$d/* do
+			echo "## [$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d2) <br>" >> "README.md"		
+			echo "## [$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d2) <br>" >> "./../$1.wiki/Home.md"			
+		done
 	fi
 done
 echo "Atualizando o readme"
