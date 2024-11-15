d60bb99b29d12251d5bfc85a23c92eb0172fb17d
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 23:55:44 -0300

    teste

--- atualizar_wiki.sh
+++ atualizar_wiki.sh
@@ -13,8 +13,8 @@ for d in ./*; do
 		for d2 in "$d/*"; do
 			# echo $d2
 			# echo $d2 | cut -d' ' -f1
-			echo "[$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d/${d2\/.\/}) <br>" >> "README.md"		
-			echo "[$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d/${d2\/.\/}) <br>" >> "./../$1.wiki/Home.md"			
+			echo "$d2 <br>" >> "README.md"		
+			echo "$d2 <br>" >> "./../$1.wiki/Home.md"			
 		done
 	fi
 done
