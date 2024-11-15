b81b079183a34c3df766577beb71fb09dda28612
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-15 00:07:23 -0300

    teste

--- atualizar_wiki.sh
+++ atualizar_wiki.sh
@@ -11,7 +11,9 @@ for d in ./*; do
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
 		cd $d
 		for d2 in ./*; do
-			echo "$d2"
+			# echo "$d2"
+			export New_string=$(echo "$d2" | tr './' '')
+			echo "Modified_string:" $New_string
 			# if [[ "$d2" != "./*"]]; then				
 				echo "$d2 <br>" >> "./../README.md"		
 				echo "$d2 <br>" >> "./../../$1.wiki/Home.md"			
