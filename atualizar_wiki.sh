d473834ebd79d04e1ee2866c18b975ad4bdd6aa6
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-14 23:23:26 -0300

    ok

--- atualizar_wiki.sh
+++ atualizar_wiki.sh
@@ -1,29 +1,16 @@
 #!/bin/bash  
 
-# $1 => repository
-
 echo "Processo de atualizacao do repositorio $1 e de sua wiki"
-
 git pull
-
-#BASE_DIRECTORY=$(dirname ./)
-#echo $BASE_DIRECTORY
-#script="teste"
-#git add * && git commit -m $1 && git push
-
 echo "# $1 <br>" > "README.md"
 echo "# $1 <br>" > "./../$1.wiki/Home.md"
-
-
 echo "Listando todos os diretorios de $1"
-
 for d in ./*; do
 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar_wiki.sh" && "$d" != "./nova_aula.sh" ]]; then
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
 	fi
 done
-
 echo "Atualizando o readme"
 git add * && git commit -m $1 && git push
 echo "Atualizando o home da wiki"
--- nova_aula.sh
+++ nova_aula.sh
@@ -1,34 +1,22 @@
 #!/bin/bash  
 
-# $1 => nome do repository
-# $2 => diretorio da aula
+# $1 => repository
+# $2 => pasta aula
 mkdir $2 && cd $2 && touch "README.md" && echo "# $2 <br>" >> "README.md" && git add * && git commit -m $2 && git push
-
 cd ./../
-
 git pull
 
 echo "Atualizando o repositorio $1 e sua wiki"
-
-#BASE_DIRECTORY=$(dirname ./)
-#echo $BASE_DIRECTORY
-#script="teste"
-#git add * && git commit -m $1 && git push
-
 echo "# $1 <br>" > "README.md"
 echo "# $1 <br>" > "./../$1.wiki/Home.md"
-
 for d in ./*; do
 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar_wiki.sh" && "$d" != "./nova_aula.sh" ]]; then
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
 	fi
 done
-
 git add * && git commit -m $1 && git push
 cd "./../$1.wiki/" && git add * && git commit -m $1 && git push
-
 echo "Processo de Atualizacao - ok"
-
 #mkdir $2 && cd $2 && touch "README.md" && echo "# $2 <br>" >> "README.md" && git add * && git commit -m $2 && git push
 
