7c32f7dca12f533d6bd2287c0d5b3e05b5e51896
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-15 00:19:46 -0300

    teste

--- atualizar_wiki.sh
+++ atualizar_wiki.sh
@@ -10,14 +10,14 @@ for d in ./*; do
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"		
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
 		cd $d				
-		echo *.*
-		# for d2 in $files; do
-		# 	# echo "$d2"
+		# echo *.*
+		for d2 in *.*; do
+			echo "$d2"
 		# 	# if [[ "$d2" != ]]; then			
 		# 		# echo "[$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../README.md"		
 		# 		# echo "[$d2](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../../$1.wiki/Home.md"			
 		# 	# fi
-		# done
+		done
 		cd ..
 	fi
 done
