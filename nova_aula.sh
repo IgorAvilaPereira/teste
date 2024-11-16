b0c9eeb03c28bb3d33f06348e94f91dc4528aa19
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-16 20:16:27 -0300

    opadfdffdgdfgdfgfd

--- nova_aula.sh
+++ nova_aula.sh
@@ -1,5 +1,3 @@
 #!/bin/bash  
 mkdir $1 && cd $1 && touch "README.md" && echo "# $1 <br>" >> "README.md" && cd ./../ && git add * && git commit -m $1 && git push 
-repositorio=basename "$PWD"
-echo $repositorio
-# ./atualizar_wiki.sh repositorio
+./atualizar_wiki.sh echo "${PWD##*/}"
