3f2e9fbd4f5d8fdb54d4206539c8262fe313a5d5
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-16 20:39:02 -0300

    ok

--- nova_aula.sh
+++ nova_aula.sh
@@ -1,4 +1,4 @@
 #!/bin/bash  
 mkdir $1 && cd $1 && touch "README.md" && echo "# $1 <br>" >> "README.md" && cd ./../ && git add * && git commit -m $1 && git push 
 # se n tiver wiki, sera preciso comentar algumas linhas do script abaixo
-./atualizar_readme.sh "${PWD##*/}"
+./atualizar.sh "${PWD##*/}"
