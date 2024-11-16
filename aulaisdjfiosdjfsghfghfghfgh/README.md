8176ab8447d965930646a32a71ea7ae7955438d3
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-16 20:06:45 -0300

    aulaisdjfiosdjfsghfghfghfgh

---
+++ aulaisdjfiosdjfsghfghfghfgh/README.md
@@ -0,0 +1,1 @@
+# aulaisdjfiosdjfsghfghfghfgh <br>
--- nova_aula.sh
+++ nova_aula.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 
-mkdir $1 && cd $1 && touch "README.md" && echo "# $1 <br>" >> "README.md" && git add * && git commit -m $1 && git push
-cd ./../
-echo "# $1 <br>" >> "README.md"
-git add * && git commit -m $1 && git push
+mkdir $1 && cd $1 && touch "README.md" && echo "# $1 <br>" >> "README.md" && cd ./../ && git add * && git commit -m $1 && git push
+# cd ./../
+# echo "# $1 <br>" >> "README.md"
+# git add * && git commit -m $1 && git push
