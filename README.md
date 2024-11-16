7d3de860d37849477aff86359aa465a79ee395d7
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-16 20:04:11 -0300

    aulaisdjfiosdjfs

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # aula14 <br>
+# aulaisdjfiosdjfs <br>
--- nova_aula.sh
+++ nova_aula.sh
@@ -1,21 +1,6 @@
 #!/bin/bash  
 
-# $1 => repository
-# $2 => pasta aula
 mkdir $1 && cd $1 && touch "README.md" && echo "# $1 <br>" >> "README.md" && git add * && git commit -m $1 && git push
 cd ./../
-# git pull
-# echo "Atualizando o repositorio $1 e sua wiki"
-echo "# $1 <br>" > "README.md"
-# echo "# $1 <br>" > "./../$1.wiki/Home.md"
-# for d in ./*; do
-# 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar_wiki.sh" && "$d" != "./nova_aula.sh" ]]; then
-# 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"
-# 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"
-# 	fi
-# done
+echo "# $1 <br>" >> "README.md"
 git add * && git commit -m $1 && git push
-# cd "./../$1.wiki/" && git add * && git commit -m $1 && git push
-echo "Processo de Atualizacao - ok"
-#mkdir $2 && cd $2 && touch "README.md" && echo "# $2 <br>" >> "README.md" && git add * && git commit -m $2 && git push
-
