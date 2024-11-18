6bcd6cb00e838fdc102f3afc79bbe4baadb8ebb6
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-17 23:19:51 -0300

    atualizando

--- atualizar.sh
+++ atualizar.sh
@@ -6,10 +6,11 @@ git push
 echo "# $1 <br>" > "README.md"
 echo "# $1 <br>" > "./../$1.wiki/Home.md"
 for d in ./*; do
+
 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar.sh" && "$d" != "./nova_aula.sh" ]]; then
-		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"		
+		echo "## [${d%%/}](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"		
 		# se n tiver wiki, favor comentar
-		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"		
+		echo "## [${d%%/}](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"		
 		cd $d							
 
 		# documentos do subdiretorio de cada aula
