1b13bf3badebefc9e97002e688d6754a774fadbb
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-11-17 23:15:50 -0300

    atualizando

--- atualizar.sh
+++ atualizar.sh
@@ -1,11 +1,10 @@
 #!/bin/bash  
 git pull
 git add *
-git commit -m 'atualizando $1'
+git commit -m 'atualizando'
 git push
 echo "# $1 <br>" > "README.md"
 echo "# $1 <br>" > "./../$1.wiki/Home.md"
-echo "Listando todos os diretorios de $1"
 for d in ./*; do
 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar.sh" && "$d" != "./nova_aula.sh" ]]; then
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "README.md"		
@@ -13,12 +12,14 @@ for d in ./*; do
 		echo "## [$d](https://github.com/IgorAvilaPereira/$1/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"		
 		cd $d							
 
+		# documentos do subdiretorio de cada aula
 		for d2 in *.*; do
 			echo "[$d2](https://github.com/IgorAvilaPereira/$1/blob/main/$d/$d2) <br>" >> "./../README.md"		
 			# se n tiver wiki, favor comentar
 			echo "[$d2](https://github.com/IgorAvilaPereira/$1/blob/main/$d/$d2) <br>" >> "./../../$1.wiki/Home.md"					
 		done
 
+		# subdiretorios internos de cada aula
 		for i in */; do 			
 			if [[ -d "$i" && ! -L "$i" ]]; then
 				echo "[${i%%/}](https://github.com/IgorAvilaPereira/$1/blob/main/$d/${i%%/}) <br>" >> "./../README.md"		
@@ -31,4 +32,4 @@ for d in ./*; do
 done
 git add * && git commit -m $1 && git push
 # se n tiver wiki, favor comentar
-cd "./../$1.wiki/" && git add * && git commit -m $1 && git push
+cd "./../$1.wiki/" && git add * && git commit -m 'atualizando' && git push
