30611d0247306476e41c89c8f789939523ab1cf4
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-12-09 17:31:05 -0300

    atualizando

--- atualizar.sh
+++ atualizar.sh
@@ -1,37 +1,37 @@
 #!/bin/bash  
 
 repositorio=${PWD##*/}
-echo repositorio
+echo $repositorio
 
 git pull
 git add *
 git commit -m 'atualizando'
 git push
 
-echo "# ${PWD##*/} <br>" > "README.md"
-echo "# ${PWD##*/} <br>" > "./../"${PWD##*/}".wiki/Home.md"
+echo "# $repositorio <br>" > "README.md"
+echo "# $repositorio <br>" > "./../$repositorio.wiki/Home.md"
 
 for d in ./*; do
 
 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar.sh" && "$d" != "./nova_aula.sh" ]]; then
-		echo "## [$d](https://github.com/IgorAvilaPereira/"${PWD##*/}"/tree/main/$d) <br>" >> "README.md"		
+		echo "## [$d](https://github.com/IgorAvilaPereira/$repositorio/tree/main/$d) <br>" >> "README.md"		
 		# se n tiver wiki, favor comentar
-		echo "## [$d](https://github.com/IgorAvilaPereira/"${PWD##*/}"/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"		
+		echo "## [$d](https://github.com/IgorAvilaPereira/$repositorio/tree/main/$d) <br>" >> "./../$1.wiki/Home.md"		
 		cd $d							
 
 		# documentos do subdiretorio de cada aula
 		for d2 in *.*; do
-			echo "[$d2](https://github.com/IgorAvilaPereira/"${PWD##*/}"/blob/main/$d/$d2) <br>" >> "./../README.md"		
+			echo "[$d2](https://github.com/IgorAvilaPereira/$repositorio/blob/main/$d/$d2) <br>" >> "./../README.md"		
 			# se n tiver wiki, favor comentar
-			echo "[$d2](https://github.com/IgorAvilaPereira/"${PWD##*/}"/blob/main/$d/$d2) <br>" >> "./../../$1.wiki/Home.md"					
+			echo "[$d2](https://github.com/IgorAvilaPereira/$repositorio/blob/main/$d/$d2) <br>" >> "./../../$1.wiki/Home.md"					
 		done
 
 		# subdiretorios internos de cada aula
 		for i in */; do 			
 			if [[ -d "$i" && ! -L "$i" ]]; then
-				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"${PWD##*/}"/blob/main/$d/${i%%/}) <br>" >> "./../README.md"		
+				echo "[${i%%/}](https://github.com/IgorAvilaPereira/$repositorio/blob/main/$d/${i%%/}) <br>" >> "./../README.md"		
 				# se n tiver wiki, favor comentar
-				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"${PWD##*/}"/blob/main/$d/${i%%/}) <br>" >> "./../../$1.wiki/Home.md"	
+				echo "[${i%%/}](https://github.com/IgorAvilaPereira/$repositorio/blob/main/$d/${i%%/}) <br>" >> "./../../$1.wiki/Home.md"	
 			fi
 		done
 		cd ..
@@ -40,4 +40,4 @@ done
 
 git add * && git commit -m 'atualizando' && git push
 # se n tiver wiki, favor comentar
-cd "./../"${PWD##*/}".wiki/" && git add * && git commit -m 'atualizando' && git push
+cd "./../$repositorio.wiki/" && git add * && git commit -m 'atualizando' && git push
