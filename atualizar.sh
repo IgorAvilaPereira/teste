afed93e9b793c0e006dbb631307393c80f461b75
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-12-09 17:49:09 -0300

    ok

--- atualizar.sh
+++ atualizar.sh
@@ -6,31 +6,31 @@ git commit -m 'atualizando'
 git push
 
 repositorio=${PWD##*/}
-echo "# $repositorio \\n" > "README.md"
-echo "# $repositorio \\n" > "./../"$repositorio".wiki/Home.md"
+echo "# $repositorio <br>" > "README.md"
+echo "# $repositorio <br>" > "./../"$repositorio".wiki/Home.md"
 
 # listando tudo interno ao repositorio
 for d in ./*; do
 
 	if [[ "$d" != "./README.md"  && "$d" != "./atualizar.sh" && "$d" != "./nova_aula.sh" ]]; then
-		echo "## [$d](https://github.com/IgorAvilaPereira/"$repositorio"/tree/main/$d) \\n" >> "README.md"		
+		echo "## [$d](https://github.com/IgorAvilaPereira/"$repositorio"/tree/main/$d) <br>" >> "README.md"		
 		# se n tiver wiki, favor comentar
-		echo "## [$d](https://github.com/IgorAvilaPereira/"$repositorio"/tree/main/$d) \\n" >> "./../"$repositorio".wiki/Home.md"		
+		echo "## [$d](https://github.com/IgorAvilaPereira/"$repositorio"/tree/main/$d) <br>" >> "./../"$repositorio".wiki/Home.md"		
 		cd $d							
 
 		# documentos do subdiretorio de cada aula
 		for d2 in *.*; do
-			echo "[$d2](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/$d2) \\n" >> "./../README.md"		
+			echo "[$d2](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/$d2) <br>" >> "./../README.md"		
 			# se n tiver wiki, favor comentar
-			echo "[$d2](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/$d2) \\n" >> "./../../"$repositorio".wiki/Home.md"					
+			echo "[$d2](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/$d2) <br>" >> "./../../"$repositorio".wiki/Home.md"					
 		done
 
 		# subdiretorios internos de cada aula
 		for i in */; do 			
 			if [[ -d "$i" && ! -L "$i" ]]; then
-				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/${i%%/}) \\n" >> "./../README.md"		
+				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/${i%%/}) <br>" >> "./../README.md"		
 				# se n tiver wiki, favor comentar
-				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/${i%%/}) \\n" >> "./../../"$repositorio".wiki/Home.md"	
+				echo "[${i%%/}](https://github.com/IgorAvilaPereira/"$repositorio"/blob/main/$d/${i%%/}) <br>" >> "./../../"$repositorio".wiki/Home.md"	
 			fi
 		done
 		cd ..
