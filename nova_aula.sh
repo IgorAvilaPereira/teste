75c41623be0f35e31d3a4e8a0bdda34b87a69854
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2024-12-09 17:42:16 -0300

    atualizando

--- nova_aula.sh
+++ nova_aula.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-./atualizar.sh "${PWD##*/}"
+./atualizar.sh
 mkdir $1 && cd $1 && touch "README.md" && echo "# $1 <br>" >> "README.md" && cd ./../ && git add * && git commit -m $1 && git push 
 # se n tiver wiki, sera preciso comentar algumas linhas do script abaixo
-./atualizar.sh "${PWD##*/}"
+./atualizar.sh 
